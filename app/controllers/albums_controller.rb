45b921640675c28ad9a345c877ad5b321f73f61e
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-07-15 15:16:34 +0300

    User can create new albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -9,10 +9,14 @@ class AlbumsController < ApplicationController
     @photos = @album.photos
   end
 
-  def new
-  end
-
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      flash[:notice] = "Album '#{@album.title}' has been successfully created"
+    else
+      flash[:alert] = "Ooops, something went wrong, album has not been saved. Repeat with other options"
+    end
+    redirect_to albums_path
   end
 
   def edit
@@ -28,5 +32,10 @@ class AlbumsController < ApplicationController
 
   def set_album
     @album = Album.find(params[:id])
+  end
+
+  def album_params
+    album = params.require(:album).permit(:title)
+    album.merge(user_id: current_user.id)
   end
 end
