4da360e06e41c7c66ec5d740d0ae97656497806d
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-07-20 12:22:26 +0300

    Fill the feed with objects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,14 @@
 class UsersController < ApplicationController
-  before_action :find_photos, only: :feed
-  before_action :find_comments, only: :feed
-  before_action :find_albums, only: :feed
   before_action :set_username
 
   def feed
     @objects = []
-    @photos.each do |p|
-      @objects << p
+    [find_photos, find_comments, find_albums].each do |obj|
+      obj.each do |o|
+	@objects << o
+      end
     end
-    @albums.each do |a|
-      @objects << a
-    end
-    @comments.each do |c|
-      @objects << c
-    end
-    @objects.sort_by(&:updated_at)
+    @objects.sort { |a,b| b.updated_at <=> a.updated_at }
   end
 
   def following
@@ -29,14 +22,46 @@ class UsersController < ApplicationController
   private
 
   def find_photos
-    @photos = find_user.photos
+    photos = []
+    followings_photos = []
+    own_photos = find_user.photos.where("updated_at > ?", not_before_date).to_a
+    find_user.following.each do |following|
+      followings_photos << following.photos.where("updated_at > ?", not_before_date).to_a
+    end
+    [own_photos, followings_photos.flatten].each do |photo|
+      photo.each { |p| photos << p }
+    end
+    photos
   end
 
   def find_comments
-    @comments = find_user.comments
+    comments = []
+    followings_comments = []
+    own_comments = find_user.comments.where("updated_at > ?", not_before_date).to_a
+    find_user.following.each do |following|
+      followings_comments << following.comments.where("updated_at > ?", not_before_date).to_a
+    end
+    [own_comments, followings_comments.flatten].each do |comment|
+      comment.each { |c| comments << c }
+    end
+    comments
   end
 
   def find_albums
-    @albums = find_user.albums
+    albums = []
+    followings_albums = []
+    own_albums = find_user.albums.where("updated_at > ?", not_before_date).to_a
+    find_user.following.each do |following|
+      followings_albums << following.albums.where("updated_at > ?", not_before_date).to_a
+    end
+    [own_albums, followings_albums.flatten].each do |album|
+      album.each { |a| albums << a }
+    end
+    albums
+  end
+
+  def not_before_date(days = 3)
+    seconds_in_one_day = 86400
+    Time.now - seconds_in_one_day * days
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def author(user)
+    if snake_case(user.full_name) == @username then 'You' else user.full_name end
+  end
 end
