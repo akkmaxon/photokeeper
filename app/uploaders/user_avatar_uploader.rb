a348d781ada631413c97ed821d5e2644d2a177fb
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-07-20 18:38:51 +0300

    Configure carrierwave for S3

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -6,11 +6,6 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
