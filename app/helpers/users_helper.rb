344099339b1f83b9a6fd3c7b29a2a872a2695fc1
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-07-20 15:43:24 +0300

    Updated view of comment in feed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,16 @@ module UsersHelper
   def author(user)
     if snake_case(user.full_name) == @username then 'You' else user.full_name end
   end
+
+  def possessive_words(comment)
+    author_of_comment = comment.user.full_name
+    author_of_photo = author(comment.photo.user)
+    if author_of_comment == author_of_photo
+      'personal'
+    elsif author_of_photo == 'You'
+      'your'
+    else
+      "#{author_of_photo}'s"
+    end
+  end
 end
