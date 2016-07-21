f68d388a2998dd467daccfdbef797f26c8ada5e1
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-07-21 18:41:03 +0300

    Default values for new photos/albums

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -2,4 +2,8 @@ module AlbumsHelper
   def current_album
     Album.find(params[:album_id])
   end
+
+  def default_album_title
+    "Album ##{current_user.albums.size + 1}"
+  end
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,9 @@
 module PhotosHelper
+  def default_photo_title
+    Time.now.strftime '%d %b %Y'
+  end
+
+  def default_photo_caption
+    "#{current_user.full_name} uploaded this photo for some reason"
+  end
 end
